792a3467b4b5b4b553b6908fd889c33979d43517
unknown <davi.cesarbt@gmail.com>
2023-03-23 10:20:57 -0300

    bit to byte converter created

--- unit-of-data-converter.py
+++ unit-of-data-converter.py
@@ -14,3 +14,17 @@ def convertByteToBit(byteQuantidy , BYTE_TO_BIT_MULTIPLIER):
 
 convertByteToBit(byteQuantidy , BYTE_TO_BIT_MULTIPLIER) ## result: 16.0 bits is equal to 2.0 bytes
 
+
+## bit to byte converter
+bitQuantidy = 16
+bitQuantidy = float(bitQuantidy)
+BIT_TO_BYTE_DIVIDER = 8
+
+def convertBitToByte(bitQuantidy, BIT_TO_BYTE_DIVIDER):
+    byteQuantidy = bitQuantidy / BIT_TO_BYTE_DIVIDER
+    byteQuantidy = float(byteQuantidy)
+    strBitQuantidy = str(bitQuantidy)
+    strByteQuantidy = str(byteQuantidy)
+    return print(strBitQuantidy + " bits " + "is equal to " + strByteQuantidy + " bytes")
+
+convertBitToByte(bitQuantidy, BIT_TO_BYTE_DIVIDER) ## result: 16.0 bits is equal to 2.0 bytes
