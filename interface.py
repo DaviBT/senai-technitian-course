787c9c6898db29b00b45555180be9c19993bd902
unknown <davi.cesarbt@gmail.com>
2023-03-23 15:51:51 -0300

    changes

---
+++ interface.py
@@ -0,0 +1,6 @@
+from unit-of-data-converter import convertByteToBit, convertBitToByte, convertByteToKb, convertKbToByte, convertKbToMb, convertMbToKb, convertMbToGb, convertGbToMb, convertGbToTb, convertTbToGb, convertTbToPb, convertPbToTb
+
+print(" - Write 1 for byte to bit \n - Write 2 for bit to byte - \n - write 3 for Byte to Kilobyte - \n - write 4 for Kilobyte to Byte - \n - write 5 for Kilobyte to Megabyte - \n - write 6 for Megabyte to Kilobyte - \n - write 7 for Megabyte to Gigabyte - \n - write 8 for Gigabyte to Megabyte - \n - write 9 for Gigabyte to Terabyte - \n - write 10 for Terabyte to Gigabyte - \n - write 11 for Terabyte to Petabyte - \n - write 12 for Petabyte to Terabyte -")
+choise = input()
+if(choise == '1'):
+    byteQuantidy = 
