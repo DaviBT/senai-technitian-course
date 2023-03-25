df8fc391e164915d6115506c967a18e82a4b1ed0
DaviBT <davi.cesarbt@gmail.com>
2023-03-24 23:38:04 -0300

    interface code chaged

--- interface.py
+++ interface.py
@@ -1,12 +1,13 @@
 from unitOfDataConverter import *
 
+input_string = input("Enter a acronym for a unit of data: ")
 def get_conversion(input_string):
     """
     Checks if string contains valid data unit conversions and returns the appropriate conversion function
     """
     # Check if the input string contains a valid conversion string
     if 'b' in input_string and 'B' in input_string:
-        return convertBitToByte
+        raise ValueError("Invalid input string. Cannot convert between bits and bytes.")
     elif 'b' in input_string:
         if 'kb' in input_string:
             return convertBitToByte, convertByteToKb
@@ -22,7 +23,7 @@ def get_conversion(input_string):
             raise ValueError("Invalid input string. Cannot determine conversion function.")
     elif 'B' in input_string:
         if 'kb' in input_string:
-            return convertByteToBit
+            return convertByteToBit, convertByteToKb
         elif 'mb' in input_string:
             return convertByteToBit, convertKbToMb
         elif 'gb' in input_string:
