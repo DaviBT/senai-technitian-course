1f7c5a9282719a22c8785859c0711d9347380f2c
unknown <davi.cesarbt@gmail.com>
2023-04-20 09:50:33 -0300

    Constant of byte that will be used to convert the units into byte created

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -5,6 +5,8 @@ quantidy = input("Enter the quantidy you want to convert: ")
 quantidyNum = float(quantidy)
 end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
 
-def converterFunction(quantidy, origin, end):
+BYTE_FACTOR = 1024
+
+def converterFunction(quantidyNum, origin, end):
     origin = units.index(origin)
     end = units.index(end)
