d1dfabf54542e8301f7d7bbd334d4a5ec38079e4
unknown <davi.cesarbt@gmail.com>
2023-04-20 13:40:21 -0300

    array created

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -1,9 +1,3 @@
-units = ["bit", "byte", "KB", "MB", "GB", "TB", "PB"]
-
-origin = input("Enter the unit of data you want to convert (example: bit, KB, GB): ")
-quantity = input("Enter the quantity you want to convert: ")
-quantityNum = float(quantity)
-end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
-
-BIT_FACTOR = 1024
+# list with all units of measurement
+units = ["bit", "byte", "Kb", "KB", "Mb", "MB", "Gb", "GB", "Tb", "TB", "Pb", "PB"]
 
