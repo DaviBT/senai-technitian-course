4c994649f85f9c7975f99b54a52c7b6f45681a87
unknown <davi.cesarbt@gmail.com>
2023-04-20 15:59:00 -0300

    code to transfer units made, bit convert still to do

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -1,3 +1,29 @@
-# list with all units of measurement
-units = ["bit", "byte", "Kb", "KB", "Mb", "MB", "Gb", "GB", "Tb", "TB", "Pb", "PB"]
+units = ["bit", "byte", "KB", "MB", "GB", "TB", "PB"]
 
+origin = input("Enter the unit of data you want to convert (example: bit, KB, GB): ")
+quantidy = input("Enter the quantidy you want to convert: ")
+quantidyNum = float(quantidy)
+end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
+
+BIT_FACTOR = 1024
+
+def converterFunction(quantidyNum, origin, end):
+    for i in units:
+        if i == origin:
+            originNum = units.index(i)
+        if i == end:
+            endNum = units.index(i)
+
+    displacement = endNum - originNum
+    positiveDisplacement = abs(displacement)
+
+    ## multiply or divide
+    if displacement < 0: ## it will multiply
+        finalQuantidy = quantidyNum * (BIT_FACTOR ** positiveDisplacement)
+        return finalQuantidy
+    
+    elif displacement > 0: # it will divide
+        finalQuantidy = quantidyNum / (BIT_FACTOR ** displacement)
+        return finalQuantidy
+
+print(converterFunction(quantidyNum, origin, end))
