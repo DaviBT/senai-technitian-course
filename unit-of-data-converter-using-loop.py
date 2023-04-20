3cdd8a47a5755af30cec1b14de4c404242755656
unknown <davi.cesarbt@gmail.com>
2023-04-20 10:29:15 -0300

    byte factor changed to bit factor

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -1,12 +1,17 @@
-units = ["bit", "byte", "KB","MB", "GB", "TB", "PB"]
+units = ["bit", "byte", "KB", "MB", "GB", "TB", "PB"]
 
 origin = input("Enter the unit of data you want to convert (example: bit, KB, GB): ")
 quantidy = input("Enter the quantidy you want to convert: ")
 quantidyNum = float(quantidy)
 end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
 
-BYTE_FACTOR = 1024
+BIT_FACTOR = 1024
 
 def converterFunction(quantidyNum, origin, end):
     origin = units.index(origin)
     end = units.index(end)
+
+    displacement = end - origin
+
+    ## transforming the unit into byte
+    initialQuantidyByte = quantidyNum * (BIT_FACTOR ** displacement)
