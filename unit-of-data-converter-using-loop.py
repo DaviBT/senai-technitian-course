93fdbbf55af9ae3bb8654611803c4a0d4d0dcf09
unknown <davi.cesarbt@gmail.com>
2023-04-20 16:30:16 -0300

    debug code

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -1,43 +1,27 @@
 units = ["bit", "byte", "KB", "MB", "GB", "TB", "PB"]
 
 origin = input("Enter the unit of data you want to convert (example: bit, KB, GB): ")
-quantidy = input("Enter the quantidy you want to convert: ")
+quantidy = input("Enter the quantity you want to convert: ")
 quantidyNum = float(quantidy)
 end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
 
 BIT_FACTOR = 1024
 
 def converterFunction(quantidyNum, origin, end):
-    for i in units:
-        if i == origin:
-            originNum = units.index(i)
-        if i == end:
-            endNum = units.index(i)
-
-    displacement = endNum - originNum
-    positiveDisplacement = abs(displacement)
-
-
-    # transforming bit to byte to transform a bit value to any other unit
-    if originNum == 0:
-        byteValue = quantidyNum / 8 
-        finalQuantidy = byteValue / (BIT_FACTOR ** displacement)
-        return finalQuantidy
+    originNum = units.index(origin)
+    endNum = units.index(end)
+    factor = 1
     
-    ## conveting any value to bit
+    if originNum > endNum:
+        factor = 1 / (BIT_FACTOR ** (originNum - endNum))
+    elif originNum < endNum:
+        factor = BIT_FACTOR ** (endNum - originNum)
+
+    byteValue = quantidyNum * factor
+    
     if endNum == 0:
-        finalQuantidy = quantidyNum * (BIT_FACTOR ** positiveDisplacement)
-        finalBitQuantidy = finalQuantidy * 8
-        return finalBitQuantidy
-
-
-    ## multiply or divide
-    if displacement < 0: ## it will multiply
-        finalQuantidy = quantidyNum * (BIT_FACTOR ** positiveDisplacement)
-        return finalQuantidy
+        return byteValue * 8
     
-    elif displacement > 0: # it will divide
-        finalQuantidy = quantidyNum / (BIT_FACTOR ** displacement)
-        return finalQuantidy
+    return byteValue / (BIT_FACTOR ** (endNum * 10))
 
 print(converterFunction(quantidyNum, origin, end))
