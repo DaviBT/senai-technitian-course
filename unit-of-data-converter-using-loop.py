374d33b51a003f155355914d6d83cc27137ae182
unknown <davi.cesarbt@gmail.com>
2023-04-20 09:42:39 -0300

    transforming the input of the quantidy into an float number

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -2,6 +2,7 @@ units = ["bit", "byte", "KB","MB", "GB", "TB", "PB"]
 
 origin = input("Enter the unit of data you want to convert (example: bit, KB, GB): ")
 quantidy = input("Enter the quantidy you want to convert: ")
+quantidyNum = float(quantidy)
 end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
 
 def converterFunction(quantidy, origin, end):
