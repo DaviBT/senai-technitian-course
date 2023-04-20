9553721de31acda8fcdc994372278d70812599fa
unknown <davi.cesarbt@gmail.com>
2023-04-20 09:40:34 -0300

    inputs created, getting the unit of data the user wants to convert, the one they want to get, and the quantidy they want to convert. The function -converterFunction- receive the input and find it in the array -units- using the index function

--- unit-of-data-converter-using-loop.py
+++ unit-of-data-converter-using-loop.py
@@ -1,2 +1,9 @@
 units = ["bit", "byte", "KB","MB", "GB", "TB", "PB"]
 
+origin = input("Enter the unit of data you want to convert (example: bit, KB, GB): ")
+quantidy = input("Enter the quantidy you want to convert: ")
+end = input("Enter the unit of data you want to get (example: byte, MB, TB): ")
+
+def converterFunction(quantidy, origin, end):
+    origin = units.index(origin)
+    end = units.index(end)
